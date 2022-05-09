edb4a5757902af9133e9c04520d187bb0d782a70
Hugo Pascoal <hugo_pascoal_@hotmail.com>
2022-05-09 09:07:03 -0300

    erro na session.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,6 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
 
-
             $table->foreign('tenant_id')
                 ->references('id')
                 ->on('tenants')
